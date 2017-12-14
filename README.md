5b0d2bf160c2e1edaa580aa0dfb5c14c53f7f63a
Zhenmao <wanzhenmao@gmail.com>
2017-12-14 13:51:54 +0800

    All cities static version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# cities-temperatures-radial-style
+# Radical Style Temperatures of Top 10 populous US cities
+
+## References
+
+Cities climate normals temperature data are extracted from [Golden Gate Weather Services](http://ggweather.com/normals/). The dataset was originally collected by [National Centers for Environmental Information's 1981-2010 U.S. Climate Normals](https://www.ncdc.noaa.gov/data-access/land-based-station-data/land-based-datasets/climate-normals/1981-2010-normals-data)
+
+[Radial Stacked Bar](https://bl.ocks.org/mbostock/6fead6d1378d6df5ae77bb6a719afcb2)
+[Sized Donut Multiples](https://bl.ocks.org/mbostock/4c5fad723c87d2fd8273)
--- js/index.js
+++ js/index.js
@@ -1,17 +1,36 @@
 /* jshint esversion: 6 */
 const width = 960,
-			height = 960;
+			height = 720;
 
-const svg = d3.select("#svg")
-	.append("svg")
-		.attr("width", width)
-		.attr("height", height);
+const citySideLength = 960 / 4,
+			cityMargin = 10;
 
-const innerRadius = 180,
-			outerRadius = Math.min(width, height) / 2;
+const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix",
+										 "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose"];
 
-const g = svg.append("g")
-		.attr("transform", `translate(${width / 2}, ${height / 2})`);
+// Clockwide layout for cities
+const citiesLayout = [
+	{ x: 0.5, y: 0.5},
+	{ x: 1.5, y: 0.5},
+	{ x: 2.5, y: 0.5},
+	{ x: 3.5, y: 0.5},
+	{ x: 3.5, y: 1.5},
+	{ x: 3.5, y: 2.5},
+	{ x: 2.5, y: 2.5},
+	{ x: 1.5, y: 2.5},
+	{ x: 0.5, y: 2.5},
+	{ x: 0.5, y: 1.5},
+];
+
+const cityCenters = cities.map((d, i) => ({
+		name: d.toLowerCase().replace(" ", "_"),
+		display: d,
+		x: citiesLayout[i].x * citySideLength,
+		y: citiesLayout[i].y * citySideLength
+	}));
+
+const innerRadius = 32,
+			outerRadius = citySideLength / 2 - cityMargin;
 
 const x = d3.scaleLinear()
 		.domain([1, 366])
@@ -22,6 +41,7 @@ const y = d3.scaleLinear()
 		.range([innerRadius, outerRadius]);
 
 const xMinMax = d3.scaleBand()
+		// .domain(data.map(d => d.day))
 		.range([0, 2 * Math.PI])
 		.align(0);
 
@@ -33,102 +53,126 @@ const color = d3.scaleOrdinal()
 		.domain(y.ticks(7))
 		.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-d3.json("data/data.json", processData);
+const svg = d3.select("#svg")
+	.append("svg")
+		.attr("width", width)
+		.attr("height", height);
 
-function processData(error, data) {
+d3.json("data/data.json", (error, data) => {
 	if (error) throw error;
-	data = data.chicago;
-	radialChart(data);
-}
 
-function radialChart(data) {
-	xMinMax.domain(data.map(d => d.day));
+	// Combine json data with cityCenters
+	cityCenters.forEach(cityCenter => {
+		cityCenter.temperatures = data[cityCenter.name];
+	});
 
-	// Minimum and Maximum temperature band
-	// g.append("g")
-	// 	.selectAll("g")
-	// 	.data(d3.stack()
-	// 			.keys(["min", "max"])
-	// 			(data))
-	// 	.enter()
-	// 	.append("g")
-	// 		.attr("fill", d => color(d.key))
-	// 	.selectAll("path")
-	// 	.data(d => d)
-	// 	.enter()
-	// 	.append("path")
-	// 		.attr("d", d3.arc()
-	// 				.innerRadius(d => y(d[0]))
-	// 				.outerRadius(d => y(d[1] - d[0]))
-	// 				.startAngle(d => xMinMax(d.data.day))
-	// 				.endAngle(d => xMinMax(d.data.day) + xMinMax.bandwidth()));
+	const citiesG = svg.append("g")
+		.attr("id", "cities")
+	.selectAll(".city")
+	.data(cityCenters)
+	.enter()
+	.append("g")
+		.attr("class", "city")
+		.attr("id", d => d.name)
+		.each(radialChart);
 
-	const yAxis = g.append("g")
-			.attr("text-anchor", "middle");
+	function radialChart(data) {
 
-	const yTick = yAxis
-		.selectAll("g")
-		.data(y.ticks(7).reverse())
-		.enter()
-		.append("g");
+		const g = d3.select(this)
+				.attr("transform", `translate(${data.x}, ${data.y})`);
 
-	yTick.append("circle")
-			.attr("fill", d => color(d))
-			.attr("stroke", "none")
-			.attr("r", y);
+		// Minimum and Maximum temperature band
+		// g.append("g")
+		// 	.selectAll("g")
+		// 	.data(d3.stack()
+		// 			.keys(["min", "max"])
+		// 			(data))
+		// 	.enter()
+		// 	.append("g")
+		// 		.attr("fill", d => color(d.key))
+		// 	.selectAll("path")
+		// 	.data(d => d)
+		// 	.enter()
+		// 	.append("path")
+		// 		.attr("d", d3.arc()
+		// 				.innerRadius(d => y(d[0]))
+		// 				.outerRadius(d => y(d[1] - d[0]))
+		// 				.startAngle(d => xMinMax(d.data.day))
+		// 				.endAngle(d => xMinMax(d.data.day) + xMinMax.bandwidth()));
 
-	yTick.append("text")
-			.attr("y", d => -y(d))
-			.attr("dy", "0.35em")
-			.attr("fill", "none")
-			.attr("stroke", "#fff")
-			.attr("stroke-width", 5)
-			.attr("stroke-linejoin", "round")
-			.text(d => d + "℉");
+		const yAxis = g.append("g")
+				.attr("text-anchor", "middle");
 
-	yTick.append("text")
-			.attr("y", d => -y(d))
-			.attr("dy", "0.35em")
-			.text(d => d + "℉");
+		const yTick = yAxis
+			.selectAll("g")
+			.data(y.ticks(7).reverse())
+			.enter()
+			.append("g");
 
-	// City name
-	g.append("text")
-			.attr("text-anchor", "middle")
-			.attr("fill", "#fff")
-			.style("font-size", "3em")
-			.attr("dy", "0.35em")
-			.text("Chicago");
+		yTick.append("circle")
+				.attr("fill", d => color(d))
+				.attr("stroke", "none")
+				.attr("r", y);
 
-	// Average temperature line
-	// g.append("g")
-	// 	.append("path")
-	// 		.datum(data)
-	// 		.attr("fill", "none")
-	// 		.attr("stroke", "#fff")
-	// 		.attr("stroke-width", 4)
-	// 		.attr("d", d3.lineRadial()
-	// 				.angle(d => x(d.day))
-	// 				.radius(d => y(d.avg)));
+		// Axis labels
+		// yTick.append("text")
+		// 		.attr("y", d => -y(d))
+		// 		.attr("dy", "0.35em")
+		// 		.attr("fill", "none")
+		// 		.attr("stroke", "#fff")
+		// 		.attr("stroke-width", 3)
+		// 		.attr("stroke-linejoin", "round")
+		// 		.attr("font-size", "0.75em")
+		// 		.text(d => d + "℉");
 
-	g.append("g")
-		.append("path")
-			.datum(data)
-			.attr("fill", "none")
-			.attr("stroke", "#fff")
-			.attr("stroke-width", 4)
-			.attr("d", d3.lineRadial()
-					.angle(d => x(d.day))
-					.radius(d => y(d.min)));
+		// yTick.append("text")
+		// 		.attr("y", d => -y(d))
+		// 		.attr("dy", "0.35em")
+		// 		.attr("font-size", "0.75em")
+		// 		.text(d => d + "℉");
 
-	g.append("g")
-		.append("path")
-			.datum(data)
-			.attr("fill", "none")
-			.attr("stroke", "#fff")
-			.attr("stroke-width", 4)
-			.attr("d", d3.lineRadial()
-					.angle(d => x(d.day))
-					.radius(d => y(d.max)));
+		// City name
+		g.append("text")
+				.attr("text-anchor", "middle")
+				.attr("fill", "#fff")
+				.style("font-size", "1em")
+				.attr("dy", "0.35em")
+				.text(data.display);
 
-}
+		// Average temperature line
+		// g.append("g")
+		// 	.append("path")
+		// 		.datum(data.temperatures)
+		// 		.attr("fill", "none")
+		// 		.attr("stroke", "#fff")
+		// 		.attr("stroke-width", 4)
+		// 		.attr("d", d3.lineRadial()
+		// 				.angle(d => x(d.day))
+		// 				.radius(d => y(d.avg)));
 
+		g.append("g")
+			.append("path")
+				.datum(data.temperatures)
+				.attr("fill", "none")
+				.attr("stroke", "#fff")
+				.attr("stroke-width", 2)
+				.attr("d", d3.lineRadial()
+						.angle(d => x(d.day))
+						.radius(d => y(d.min)));
+
+		g.append("g")
+			.append("path")
+				.datum(data.temperatures)
+				.attr("fill", "none")
+				.attr("stroke", "#fff")
+				.attr("stroke-width", 2)
+				.attr("d", d3.lineRadial()
+						.angle(d => x(d.day))
+						.radius(d => y(d.max)));
+
+	}
+
+
+
+
+});
